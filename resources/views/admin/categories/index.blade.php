89b559bbe4253791713ae2e3561fe8389325755d
Racuwcka <13zhora13@mail.ru>
2022-12-13 22:37:17 +0400

    creating sidebar admin and route category

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,18 @@
+@extends('admin.layouts.main')
+
+@section('content')
+
+<section class="content">
+    <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+            <div class="col-12">
+                Категории
+            </div>
+        </div>
+        <!-- /.row -->
+
+    </div><!-- /.container-fluid -->
+</section>
+
+@endsection
---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,20 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+                 with font-awesome or any other icon font library -->
+                <a href="{{ route('admin.category.index') }}" class="nav-link">
+                    <i class="nav-icon far fa-image"></i>
+                    <p>
+                        Gallery
+                    </p>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!-- /.sidebar -->
+</aside>
