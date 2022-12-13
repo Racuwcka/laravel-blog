b244388ac907916fe6165c516bc7e053f23a7d5d
Racuwcka <13zhora13@mail.ru>
2022-12-13 11:15:26 +0400

    create adminPanel

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,11 @@ Route::group(['namespace' => 'App\Http\Controllers\Main'], function() {
     Route::get('/', 'IndexController')->name('post.index');
 });
 
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function() {
+    Route::group(['namespace' => 'Main'], function() {
+        Route::get('/', 'IndexController')->name('post.index');
+    });
+});
+
 Auth::routes();
 
