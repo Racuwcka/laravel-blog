454381becdef4646a92e758cacbdf52893b75e4c
Racuwcka <13zhora13@mail.ru>
2022-12-17 18:59:01 +0400

    creating Tag's CRUD

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -4,13 +4,22 @@
 
     <!-- Sidebar -->
     <div class="sidebar">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <ul class="nav nav-pills nav-sidebar flex-column mt-2" data-widget="treeview" role="menu" data-accordion="false">
             <!-- Add icons to the links using the .nav-icon class
                  with font-awesome or any other icon font library -->
+            <li class="nav-item">
                 <a href="{{ route('admin.category.index') }}" class="nav-link">
                     <i class="nav-icon far fa-image"></i>
                     <p>
                         Категории
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ route('admin.tag.index') }}" class="nav-link">
+                    <i class="nav-icon far fa-image"></i>
+                    <p>
+                        Теги
                     </p>
                 </a>
             </li>
--- resources/views/admin/tag/index.blade.php
+++ resources/views/admin/tag/index.blade.php
@@ -8,7 +8,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Категории</h1>
+                        <h1 class="m-0">Теги</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
@@ -27,7 +27,7 @@
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
                     <div class="col-1 mb-3">
-                        <a href="{{ route('admin.category.create') }}" class="btn btn-block btn-primary">Добавить</a>
+                        <a href="{{ route('admin.tag.create') }}" class="btn btn-block btn-primary">Добавить</a>
                     </div>
                 </div>
                 <div class="row">
@@ -44,16 +44,16 @@
                                     </tr>
                                     </thead>
                                     <tbody>
-                                    @foreach($categories as $category)
+                                    @foreach($tags as $tag)
                                         <tr>
-                                            <td>{{ $category->id }}</td>
-                                            <td>{{ $category->title }}</td>
-                                            <td class="text-center"><a href="{{ route('admin.category.show', $category->id) }}"><i
+                                            <td>{{ $tag->id }}</td>
+                                            <td>{{ $tag->title }}</td>
+                                            <td class="text-center"><a href="{{ route('admin.tag.show', $tag->id) }}"><i
                                                         class="fas fa-eye"></i></a></td>
-                                            <td class="text-center"><a href="{{ route('admin.category.edit', $category->id) }}"
+                                            <td class="text-center"><a href="{{ route('admin.tag.edit', $tag->id) }}"
                                                    class="text-success"><i class="fas fa-pen"></i></a></td>
                                             <td class="text-center">
-                                                <form action="{{ route('admin.category.delete', $category->id) }}"
+                                                <form action="{{ route('admin.tag.delete', $tag->id) }}"
                                                       method="Post">
                                                     @csrf
                                                     @method('DELETE')
