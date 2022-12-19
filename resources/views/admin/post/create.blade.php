2fd2ec5ef2a0a8f3bbbb45be5b8b50c7d4718601
Racuwcka <13zhora13@mail.ru>
2022-12-20 00:35:42 +0400

    categories and tags in creating post

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -45,7 +45,7 @@
                                 @enderror
                             </div>
                             <div class="form-group w-50">
-                                <label for="exampleInputFile">Добавить превью</label>
+                                <label>Добавить превью</label>
                                 <div class="input-group">
                                     <div class="custom-file">
                                         <input type="file" class="custom-file-input" name="preview_image">
@@ -55,9 +55,12 @@
                                         <span class="input-group-text">Загрузить</span>
                                     </div>
                                 </div>
+                                @error('preview_image')
+                                <div class="text-danger">Это поле необходимо для заполнения</div>
+                                @enderror
                             </div>
                             <div class="form-group w-50">
-                                <label for="exampleInputFile">Добавить главное изображение</label>
+                                <label>Добавить главное изображение</label>
                                 <div class="input-group">
                                     <div class="custom-file">
                                         <input type="file" class="custom-file-input" name="main_image">
@@ -67,6 +70,29 @@
                                         <span class="input-group-text">Загрузить</span>
                                     </div>
                                 </div>
+                                @error('main_image')
+                                <div class="text-danger">Это поле необходимо для заполнения</div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <label>Выберите категорию</label>
+                                <select name="category_id" class="form-control">
+                                    @foreach($categories as $category)
+                                        <option value="{{ $category->id }}"
+                                        {{ $category->id == old('category_id') ? ' selected' : '' }}
+                                        >{{ $category->title }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group w-50">
+                                <label>Теги</label>
+                                <select class="select2" name="tag_ids[]" multiple="multiple" data-placeholder="Выберите теги" style="width: 100%;">
+                                    @foreach($tags as $tag)
+                                        <option value="{{ $tag->id }}"
+                                        {{ is_array(old('tag_ids')) && in_array($tag->id, old('tag_ids')) ? ' selected' : '' }}
+                                        >{{ $tag->title }}</option>
+                                    @endforeach
+                                </select>
                             </div>
                             <div class="form-group">
                                 <input type="submit" class="btn btn-primary" value="Добавить">
