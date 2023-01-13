44398148591f5fbfcf64bf11f67cd3b8f721f601
Racuwcka <13zhora13@mail.ru>
2023-01-13 17:53:29 +0400

    telescope

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -15,6 +15,8 @@ class Comment extends Model
     protected $table = 'comments';
     protected $guarded = false;
 
+    protected $with = ['user'];
+
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
